81f3151548b4709a0a27bdc8343772520db67f30
Jgr76 <jgr76@scarletmail.rutgers.edu>
2017-10-11 19:52:03 -0400

    Basic Scripts
    Added the basic scripts from class. To be ended in detail soon

---
+++ Midterm_Project/Assets/_Scripts/twoDamageObstacle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twoDamageObstacle : MonoBehaviour {
+
+	private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if (other.gameObject.tag == "Player") {
+
+			player.hurt = true;
+			player.Damage (2);
+
+		} else {
+			player.hurt = false;
+		}
+	}
+
+	// Update is called once per frame
+	void OnTriggerExit2d (Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			player.hurt = false;
+
+		}
+	}
+}
