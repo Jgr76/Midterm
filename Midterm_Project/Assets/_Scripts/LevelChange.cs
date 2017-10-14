dd7771c81246ef8ec7046b1a971d9e4211b12b47
Jgr76 <jgr76@scarletmail.rutgers.edu>
2017-10-14 15:04:03 -0400

    Snow platforms

--- Midterm_Project/Assets/_Scripts/LevelChange.cs
+++ Midterm_Project/Assets/_Scripts/LevelChange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelChange : MonoBehaviour {
+public class LevelChange2 : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other){
 
